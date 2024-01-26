327dc634730b1c658c8d02a7aa38d6035bdf4a30
bfmunozm <swsbmm@gmail.com>
2024-01-26 06:06:36 -0500

    feat: udistrital/sga_documentacion#22 Refactorización imports, gitignore, nombre endpoints

--- tests/default_test.go
+++ tests/default_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"runtime"
 	"path/filepath"
-	_ "sga_mid_notas/routers"
+	_ "github.com/udistrital/sga_mid_notas/routers"
 
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
